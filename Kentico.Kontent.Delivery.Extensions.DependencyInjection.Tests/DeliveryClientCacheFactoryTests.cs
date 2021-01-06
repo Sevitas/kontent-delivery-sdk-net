0c83d09748affc74cc9ef77c3de4331c0710a4f0
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2021-01-06 21:51:22 +0100

    extensions package for a named client

---
+++ Kentico.Kontent.Delivery.Extensions.DependencyInjection.Tests/DeliveryClientCacheFactoryTests.cs
@@ -0,0 +1,59 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.Caching;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kentico.Kontent.Delivery.Extensions.DependencyInjection.Tests
+{
+    public class DeliveryClientCacheFactoryTests
+    {
+        private readonly IOptionsMonitor<DeliveryCacheOptions> _deliveryCacheOptionsMock;
+        private readonly IDeliveryClientFactory _innerDeliveryClientFactoryMock;
+        private readonly IServiceProvider _serviceProvider;
+
+        private const string _clientName = "ClientName";
+
+        public DeliveryClientCacheFactoryTests()
+        {
+            _deliveryCacheOptionsMock = A.Fake<IOptionsMonitor<DeliveryCacheOptions>>();
+            _innerDeliveryClientFactoryMock = A.Fake<IDeliveryClientFactory>();
+            _serviceProvider = new ServiceCollection().BuildServiceProvider();
+        }
+
+        [Fact]
+        public void GetNamedCacheClient_WithCorrectName_GetClient()
+        {
+            var deliveryCacheOptions = new DeliveryCacheOptions();
+            A.CallTo(() => _deliveryCacheOptionsMock.Get(_clientName))
+                .Returns(deliveryCacheOptions);
+
+            var deliveryClientFactory = new DeliveryClientCacheFactory(_innerDeliveryClientFactoryMock, _deliveryCacheOptionsMock, _serviceProvider);
+
+            var result = deliveryClientFactory.Get(_clientName);
+
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void GetNamedCacheClient_WithWrongName_GetNull()
+        {
+            var deliveryCacheOptions = new DeliveryCacheOptions();
+            A.CallTo(() => _deliveryCacheOptionsMock.Get(_clientName))
+                .Returns(deliveryCacheOptions);
+
+            var deliveryClientFactory = new DeliveryClientCacheFactory(_innerDeliveryClientFactoryMock, _deliveryCacheOptionsMock, _serviceProvider);
+
+            var result = deliveryClientFactory.Get("WrongName");
+
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ Kentico.Kontent.Delivery.Extensions.DependencyInjection.Tests/DeliveryClientFactoryTests.cs
@@ -0,0 +1,52 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Kentico.Kontent.Delivery.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using Xunit;
+
+namespace Kentico.Kontent.Delivery.Extensions.DependencyInjection.Tests
+{
+    public class DeliveryClientFactoryTests
+    {
+        private readonly IOptionsMonitor<DeliveryOptions> _deliveryOptionsMock;
+        private readonly IServiceProvider _serviceProvider;
+
+        private const string _clientName = "ClientName";
+
+        public DeliveryClientFactoryTests()
+        {
+            _deliveryOptionsMock = A.Fake<IOptionsMonitor<DeliveryOptions>>();
+            _serviceProvider = new ServiceCollection().BuildServiceProvider();
+        }
+
+        [Fact]
+        public void GetNamedClient_WithCorrectName_GetClient()
+        {
+            var deliveryOptions = new DeliveryOptions() { ProjectId = Guid.NewGuid().ToString() };
+            A.CallTo(() => _deliveryOptionsMock.Get(_clientName))
+                .Returns(deliveryOptions);
+
+            var deliveryClientFactory = new DeliveryClientFactory(_deliveryOptionsMock, _serviceProvider);
+
+            var result = deliveryClientFactory.Get(_clientName);
+
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void GetNamedClient_WithWrongName_GetNull()
+        {
+            var deliveryOptions = new DeliveryOptions() { ProjectId = Guid.NewGuid().ToString() };
+            A.CallTo(() => _deliveryOptionsMock.Get(_clientName))
+                .Returns(deliveryOptions);
+
+            var deliveryClientFactory = new DeliveryClientFactory(_deliveryOptionsMock, _serviceProvider);
+
+            var result = deliveryClientFactory.Get("WrongName");
+
+            result.Should().BeNull();
+        }
+    }
+}
